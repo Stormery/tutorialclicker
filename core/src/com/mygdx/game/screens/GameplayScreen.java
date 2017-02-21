9cdc2672bfc0bd7be231909dedc85261c2127a53
Ayo-PC\Ayo <panstormery@gmail.com>
2017-02-21 13:50:51 +0000

    refactor ScoreLabel

--- core/src/com/mygdx/game/screens/GameplayScreen.java
+++ core/src/com/mygdx/game/screens/GameplayScreen.java
@@ -1,26 +1,24 @@
 package com.mygdx.game.screens;
 
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.mygdx.game.TutorialClickerGame;
 import com.mygdx.game.entities.Player;
+import com.mygdx.game.ui.IClickCallback;
+import com.mygdx.game.ui.PlayerButton;
+import com.mygdx.game.ui.ResetScoreButton;
+import com.mygdx.game.ui.ScoreLabel;
 
 public class GameplayScreen extends AbstractScreen{
 	
 	private Player player;
-	private Button playerButton, resetScoreButton;
-	private Label scoreLabel;
+	private PlayerButton playerButton;
+	private ResetScoreButton resetScoreButton;
+	private ScoreLabel scoreLabel;
 
 	public GameplayScreen(TutorialClickerGame game) {
 		super(game);
 	}
 	
-	@Override
+	@Override	
 	protected void init() {
 		initPlayer();
 		initPlayerButton();
@@ -29,59 +27,33 @@ public class GameplayScreen extends AbstractScreen{
 	}
 	
 	private void initResetScoreButton() {
-		resetScoreButton = new Button(new ButtonStyle());
-		resetScoreButton.setWidth(100);
-		resetScoreButton.setHeight(100);
-		resetScoreButton.setX(330);
-		resetScoreButton.setY(560);
-		resetScoreButton.setDebug(true);
+		resetScoreButton = new ResetScoreButton(new IClickCallback() {
+			
+			@Override
+			public void onClick() {
+				game.resetGameScore();
+			}
+		});
 		
 		stage.addActor(resetScoreButton);
 		
-		resetScoreButton.addListener(new ClickListener(){
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y,
-					int pointer, int button) {
-				
-				game.resetGameScore();
-				
-				return super.touchDown(event, x, y, pointer, button);
-			}
-		});
 	}
 
 	private void initScoreLabel() {
-		LabelStyle labelStyle = new LabelStyle();
-		labelStyle.font = new BitmapFont();
-		scoreLabel = new Label("", labelStyle);
-		scoreLabel.setX(20);
-		scoreLabel.setY(650);
+		scoreLabel = new ScoreLabel();
 		stage.addActor(scoreLabel);
 	}
 
 	private void initPlayerButton() {
-		playerButton = new Button(new ButtonStyle());
-		playerButton.setWidth(460);
-		playerButton.setHeight(360);
-		playerButton.setX(10);
-		playerButton.setY(170);
-		playerButton.setDebug(true);
-		
-		stage.addActor(playerButton);
-		
-		playerButton.addListener(new ClickListener(){
-			
+		playerButton = new PlayerButton(new IClickCallback() {
 			@Override
-			public boolean touchDown(InputEvent event, float x, float y,
-					int pointer, int button) {
-				
+			public void onClick() {
 				player.reactOnClick();
 				game.addPoint();
-				
-				return super.touchDown(event, x, y, pointer, button);
 			}
-			
 		});
+		
+		stage.addActor(playerButton);
 	}
 
 	private void initPlayer() {
---
+++ core/src/com/mygdx/game/ui/ScoreLabel.java
@@ -0,0 +1,25 @@
+package com.mygdx.game.ui;
+
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+
+public class ScoreLabel extends Label{
+
+	public ScoreLabel() {
+
+		super("", prepareLabelStyle());
+		init();
+	}
+
+	private void init() {
+		this.setX(20);
+		this.setY(650);
+	}
+
+	private static LabelStyle prepareLabelStyle() {
+
+		LabelStyle labelStyle = new LabelStyle();
+		labelStyle.font = new BitmapFont();
+		return labelStyle;
+	}
+}
